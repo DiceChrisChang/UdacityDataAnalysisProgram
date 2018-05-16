e59e725dc3be9c63d108965ba6cebaf50445d8bf
Dice <ChrisChangDice@163.com>
2018-05-16 19:10:35 +0800

    final

--- part2/udacity_bikeshare-new/filter_data.py
+++ part2/udacity_bikeshare-new/filter_data.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import pandas as pd
 import matplotlib
@@ -7,7 +6,6 @@ import seaborn as sns
 # how to use seaborn
 from matplotlib.figure import Figure
 # for the plot visualization
-
 
 def filter_data(city,month,weekday):
     '''function to get data from csv files devided by city
@@ -21,24 +19,34 @@ def filter_data(city,month,weekday):
     # CITY_DATA is dict so use items() to traverse/遍历 to get keys & values
     for (key,values) in CITY_DATA.items():
         if key==city:
-            data = pd.read_csv(values)
-    data['Start Time'] = pd.to_datetime(data['Start Time'])
-    data['start_month'] = data['Start Time'].dt.month
-    data['start_hour'] = data['Start Time'].dt.hour
-    data['start_weekday'] = data['Start Time'].dt.weekday
-    if month != None:
-        months = ['January', 'February', 'March', 'April', 'May', 'June',
-                  'July','Augest','September','October ','November','December']
+            df = pd.read_csv(values)
+    df['Start Time'] = pd.to_datetime(df['Start Time'])
+    df['start_month'] = df['Start Time'].dt.month
+    df['start_hour'] = df['Start Time'].dt.hour
+    df['start_weekday'] = df['Start Time'].dt.weekday
+    months = ['January', 'February', 'March', 'April', 'May', 'June',
+              'July', 'Augest', 'September', 'October', 'November', 'December']
+    weeks = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
+    # weekday start from 0 to 6 as the indexs don't need to add 1
+    if month != None and weekday != None:
         start_month = months.index(month) + 1
-        data = data[data['start_month'] == start_month]
-    if weekday != None:
-        weeks = ['Sunday','Monday','Tuesday','Wednesday ','Thursday ','Friday ','Saturday']
-        # weekday start from 0 to 6 as the indexs don't need to add 1
         start_weekday = weeks.index(weekday)
-        data = data[data['start_weekday'] == start_weekday]
+        df = df[df['start_month'] == start_month]
+        df = df[df['start_weekday'] == start_weekday]
+        return df
+    elif month != None and weekday == None:
+        start_month = months.index(month) + 1
+        df = df[df['start_month'] == start_month]
+        return df
+    elif month == None and weekday != None:
+        start_weekday = weeks.index(weekday)
+        df = df[df['start_weekday'] == start_weekday]
+        # print(data)
+        return df
+    else:
+        df = None
+        return df
     # in order to anlysis data
-    return data
-
 
 def data_analysis(data,two_choosen_parts):
     mode_name = data[two_choosen_parts].value_counts().first_valid_index()
@@ -46,8 +54,8 @@ def data_analysis(data,two_choosen_parts):
     return mode_name,mode_count
 
 def trip_dutation(data):
-    trip_dutation_sum = data['Trip Duration'].sum()
-    trip_dutation_mean = data['Trip Duration'].mean()
+    trip_dutation_sum = ['sum',data['Trip Duration'].sum()]
+    trip_dutation_mean =['mean',data['Trip Duration'].mean()]
     return trip_dutation_sum,trip_dutation_mean
 
 def user_analysis(data,three_parts_user):
@@ -55,86 +63,86 @@ def user_analysis(data,three_parts_user):
         total_type_analysis = data[three_parts_user].value_counts()
         return total_type_analysis
     elif three_parts_user == 'Birth Year':
-        year_min = data[three_parts_user].min()
-        year_max = data[three_parts_user].max()
-        year_mode = data[three_parts_user].value_counts().first_valid_index()
+        year_min = ['min',data[three_parts_user].min()]
+        year_max = ['max',data[three_parts_user].max()]
+        year_mode = ['mode',data[three_parts_user].value_counts().first_valid_index()]
         return year_min,year_max,year_mode
 
 def filters():
     '''
     get values from input and use filter_data to filter return a data seperate datetime
     '''
+    global data
     try:
         city = input('\nWhich city do you want to walk through ? Chicago, NewYorkCity, Washington \n')
-        month = input('\nWhich month January,February,March,Apri,May,June,July,Augest,September,October,November,December ? \n')
-        weekday = input('\nWhich weekday Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday \n')
+        three_choices = input('\nWould you like to filter datas from months,weekdays or both, m for months, w for weekdays, b for both \n')
+        if three_choices == 'b':
+            month = input('\nWhich month January,February,March,April,May,June,July,Augest,September,October,November,December ? \n')
+            weekday = input('\nWhich weekday Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday \n')
+        elif three_choices == 'w':
+            weekday = input('\nWhich weekday Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday \n')
+            month = None
+        elif three_choices == 'm':
+            month = input('\nWhich month January,February,March,April,May,June,July,Augest,September,October,November,December ? \n')
+            weekday = None
         data = filter_data(city,month,weekday)
     except:
         print('There is something wrong in your input, plesase try again! ')
         filters()
     else:
-        # data = filter_data(city,month,weekday)
-        return data
-
-def analysis_station():
-    try:
-        choose = input('\n Woule you like to watch data by start/end station or combine both, you can enter s for one or enter b for both? \n')
-        if choose == 's':
-            two_choosen_parts = input('\n input Start Station or End Station \n')
-            data_analysis(data,two_choosen_parts)
-        elif choose == 'b':
-            #### find from start station to end station the most popular trip ####
-            combine_station_data = data.groupby(['Start Station','End Station']).size() \
-            .reset_index(name='count').sort_values(by = ['count'], ascending = False).iloc[0,]
-    except:
-        print('There is something wrong in your input, plesase try again! ')
-        analysis_station()
-    else:
-        if choose == 's':
-            print('')
-        elif choose == 'b':
-            print('')
-
-def personal_info():
-    try:
-        three_parts_user = input('\n Which kind of data do you want from User Type, Gender, Birth Year ? \n')
-        user_analysis(data,three_parts_user)
-    except:
-        print('There is something wrong in your input, plesase try again! ')
-        personal_info()
-    else:
-        print(user_analysis(data,three_parts_user))
-        retry = input('\n Do you want to see other kinds of info ? Yes or No \n')
-        if retry == "Yes" :
-           personal_info()
-        else:
-            break
-
-########################### main ##############################
-
-# print(data.groupby(['Start Station','End Station']).agg({'Start Station':sum,'End Station':sum}))
-
-
-# three_parts_user = ['User Type','Gender','Birth Year']
-# user_analysis(data,'User Type')
-# seems like some city file don't have the user type
-
-global data
-def main ():
-    while True:
-        filters()
-        if data.empty:
+        if data.empty or data is None:
             print('This kind of date that you choosen is empty, please try again')
             filters()
 
-        analysis_station()
+    return data,city
 
-        print('I am going to show you the calculate  time of whole trip by bike ')
+def analysis_station(data):
+    # try:
+        choose = input('\nWoule you like to watch data by start/end station or combine both, you can enter s for one or enter b for both? \n')
+        if choose == 's':
+            two_choosen_parts = input('\ninput Start Station or End Station \n')
+            data_analysis(data,two_choosen_parts)
+        elif choose == 'b':
+            #### find the most popular trip from start station to end station ####
+            combine_station_data = data.groupby(['Start Station','End Station']).size() \
+            .reset_index(name='count').sort_values(by = ['count'], ascending = False).iloc[0,]
+    # except:
+    #     print('There is something wrong in your input, plesase try again! ')
+    #     analysis_station(data)
+    # else:
+        if choose == 's':
+            print(data_analysis(data,two_choosen_parts))
+        elif choose == 'b':
+            print(combine_station_data)
+
+def personal_info(data,city):
+        if city == 'Washington':
+            print('I am going to show you the calculate user type of Washington city by bike ')
+            print(user_analysis(data,'User Type'))
+        else:
+            three_parts_user = input('\nWhich kind of data do you want from User Type, Gender, Birth Year ? \n')
+            print(user_analysis(data,three_parts_user))
+            retry = input('\nDo you want to see other kinds of info ? Yes or No \n')
+            if retry.lower() == "yes" :
+                personal_info(data,city)
+            else:
+                return
+
+########################### main ##############################
+global data
+def main ():
+    while True:
+        data,city = filters()
+
+        analysis_station(data)
+
+        print('I am going to show you the calculate time of whole trip by bike ')
         print(trip_dutation(data))
         # data = filter_data('Chicago','March','Monday')
 
-        personal_info()
-        restart = input('\nWould you like to restar ? Enter yes or no.\n')
+        personal_info(data,city)
+
+        restart = input('\nWould you like to restar ? Enter Yes or No.\n')
         if restart.lower() != 'yes':
             break
 
--- part2/udacity_bikeshare-new/readme.txt
+++ part2/udacity_bikeshare-new/readme.txt
@@ -7,4 +7,7 @@
 6. https://stackoverflow.com/questions/17679089/pandas-dataframe-groupby-two-columns-and-get-counts
 7. https://stackoverflow.com/questions/17995024/how-to-assign-a-name-to-the-a-size-column
 8. http://pandas.pydata.org/pandas-docs/version/0.19.2/generated/pandas.DataFrame.sort_values.html#pandas.DataFrame.sort_values
-9. 
+9. http://www.itkeyword.com/doc/9836292375735051067/how-to-compare-pandas-dataframe-against-none-in-python
+10. https://stackoverflow.com/questions/23086383/how-to-test-nonetype-in-python
+11. https://stackoverflow.com/questions/9752958/how-can-i-return-two-values-from-a-function-in-python
+12. https://blog.csdn.net/joeblackzqq/article/details/35278665
